6fca2c4b39ca885b124acb903c31f85c6a4896ce
rrgayhart <rrgayhart@gmail.com>
2017-04-13 14:25:08 -0600

    Add ability to shake

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-// Find buy button
-
 var submitButton = document.getElementById('snack-submit');
 var inputField = document.getElementById('snack-input');
 var message = document.getElementById('flash-message');
 var purchases = document.getElementById('snack-purchases');
+var snackCollection = document.getElementById('snack-collection');
 
-submitButton.addEventListener('click', function(event){
+submitButton.addEventListener('click', function(){
   var value = inputField.value;
   var snack = document.getElementById(value);
   if(snack) {
@@ -15,4 +14,8 @@ submitButton.addEventListener('click', function(event){
   } else {
     message.textContent = 'Sorry! I am out of that!';
   }
+});
+
+shake.addEventListener('click', function(){
+  snackCollection.classList.toggle('shake');
 });
