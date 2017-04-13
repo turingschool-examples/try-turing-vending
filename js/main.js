e49e9f264f822d2655864a289214fb9026b14d81
rrgayhart <rrgayhart@gmail.com>
2017-04-13 13:49:17 -0600

    Add to purchases

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@
 var submitButton = document.getElementById('snack-submit');
 var inputField = document.getElementById('snack-input');
 var message = document.getElementById('flash-message');
+var purchases = document.getElementById('snack-purchases');
 
 submitButton.addEventListener('click', function(event){
   var value = inputField.value;
   var snack = document.getElementById(value);
   if(snack) {
-    snack.classList.add('hide')
+    purchases.appendChild(snack);
   } else {
     message.textContent = 'Sorry! I am out of that!';
   }
